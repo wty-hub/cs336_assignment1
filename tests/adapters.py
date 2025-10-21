a61b68d7b94d592e6633c79647152978528a7379
wty <623417407@qq.com>
2025-10-21 21:56:34 +0800

    update

--- tests/adapters.py
+++ tests/adapters.py
@@ -9,8 +9,8 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
-from cs336_basics.tokenizer import Tokenizer
-from cs336_basics.train_bpe import optimized_train_bpe, train_bpe
+from cs336_basics.bpe.tokenizer import Tokenizer
+from cs336_basics.bpe.train_bpe import optimized_train_bpe, train_bpe
 
 
 def run_linear(
